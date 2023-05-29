4453f430bffc70581f9213fa9628f32321920a7f
Atiq078 <131598148+Atiq078@users.noreply.github.com>
2023-05-30 01:16:00 +0500

    Update server.js

--- back/server.js
+++ back/server.js
@@ -58,7 +58,7 @@ require('./app/routes/user.routes')(app);
 /////// login registeration /////////
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
